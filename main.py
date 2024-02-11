f5b49a202b6ed322cb8c681f4cdda96c2aee22e9
Gurucharan S <70633240+GurucharanSavanth@users.noreply.github.com>
2024-02-11 09:45:15 +0000

    Optimized the code with implementation of streamlit

--- main.py
+++ main.py
@@ -10,88 +10,52 @@ import streamlit as st
 
 ## Downlode https://s3.amazonaws.com/google-landmark/train/images_345.tar
 ## You can use different Sources Eg. Local Path or online dataset model and csv model as well  
-TF_MODEL_URL = 'https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_asia_V1/1'
-LABEL_MAP_URL = 'https://www.gstatic.com/aihub/tfhub/labelmaps/landmarks_classifier_asia_V1_label_map.csv'
-IMAGE_SHAPE = (321, 321)
-
-classifier = tf.keras.Sequential([hub.KerasLayer(TF_MODEL_URL,
-                                                 input_shape=IMAGE_SHAPE + (3,),
-                                                 output_key="predictions:logits")])
-
+# Load model and label map
+classifier = tf.keras.Sequential([hub.KerasLayer(TF_MODEL_URL, input_shape=IMAGE_SHAPE + (3,), output_key="predictions:logits")])
 df = pd.read_csv(LABEL_MAP_URL)
 label_map = dict(zip(df.id, df.name))
 
-img_loc = [ "/content/b0b2d0916f625f4a.jpg","/content/b0b2db10da6e09c9.jpg"]
-for img_loc in img_loc:
-    img = Image.open(img_loc).resize(IMAGE_SHAPE)
-    print(img)
+# Function to process and predict image
+def process_and_predict_image(img_path):
+    img = Image.open(img_path).resize(IMAGE_SHAPE)
+    img_array = np.array(img) / 255.0
+    img_array = img_array[np.newaxis, ...]
+    result = classifier.predict(img_array)
+    predicted_label = label_map[np.argmax(result)]
+    return img_array, predicted_label
 
-
-img = np.array(img) / 255.0
-print(img.shape)
-
-img = img[np.newaxis, ...]
-print(img.shape)
-
-result = classifier.predict(img)
-print(result)
-
-var = label_map[np.argmax(result)]
-class_names = list(label_map.values())
-print(var)
-print(class_names)
-
-## Squeeze the image array to remove the batch dimension
-img = np.squeeze(img)
-
-# Plot the image
-fig, ax = plt.subplots()
-ax.imshow(img)
-
-# Plot the predicted landmark
-landmark = label_map[np.argmax(result)]
-ax.scatter(landmark[0], landmark[1], color='red', marker='o')
-
-# Set the title and labels
-ax.set_title('Predicted landmark: {}'.format(landmark))
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-
-# Show the plot
-plt.show()
-
-# Assuming that the model, preprocessor, and label_map are defined
-
-def classify_image(image):
-    img = np.array(image) / 255.0
-    img = img[np.newaxis, ...]
-    prediction = classifier.predict(img)
-    return label_map[np.argmax(prediction)]
-
+# Streamlit App
 st.title('Landmark Prediction App')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file).resize(IMAGE_SHAPE)
     st.image(image, caption='Input Image', use_column_width=True)
-    prediction = classify_image(image)
+    processed_image, prediction = process_and_predict_image(uploaded_file)
     st.write(f"Predicted Landmark: {prediction}")
-image = components.Image()
-label = components.Label(num_top_classes=1)
+
+    # Plot the image with prediction
+    fig, ax = plt.subplots()
+    ax.imshow(np.squeeze(processed_image))
+    ax.set_title(f'Predicted landmark: {prediction}')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    st.pyplot(fig)
+
+# Gradio Interface
+def classify_image(image):
+    img = Image.fromarray(image.astype('uint8'), 'RGB').resize(IMAGE_SHAPE)
+    img_array = np.array(img) / 255.0
+    img_array = img_array[np.newaxis, ...]
+    prediction = classifier.predict(img_array)
+    return label_map[np.argmax(prediction)]
+
+image_input = gr.components.Image()
+label_output = gr.components.Label(num_top_classes=1)
 
 gr.Interface(
     classify_image,
-    inputs=image,
-    outputs=label
+    inputs=image_input,
+    outputs=label_output
 ).launch(share=True)
-'''You can use the 
-gr.Interface(
-    classify_image,
-    inputs=image,
-    outputs=label,
-).launch(share=True) for Generally sharing the link which can be used as well 
-and 
-
-(Debug=True) for debugging purpous 
-
-
-'''
+# to run the program using streamlit
+# streamlit run colab_kernel_launcher.py
